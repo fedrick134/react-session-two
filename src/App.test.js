869de9cc023bb3af87a4265a4c6e3416116575f6
fedricknishant <fednish@gmail.com>
2023-04-14 21:12:06 +0530

    8

--- src/App.test.js
+++ src/App.test.js
@@ -6,5 +6,5 @@ test('renders learn react link', () => {
   const linkElement = screen.getByText(/learn react/i);
   expect(linkElement).toBeInTheDocument();
 
-  expect(1).toBe(7)
+  expect(1).toBe(8)
 });
